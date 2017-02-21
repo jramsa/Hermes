289041e80cd063a24fda98cf7e2c5a65b59fcfcb
jramsa <jerome.ramsamy@hotmail.fr>
2017-02-21 18:26:51 +0100

    l

--- src/java/beans/PatientBean.java
+++ src/java/beans/PatientBean.java
@@ -42,7 +42,7 @@ public class PatientBean implements Serializable {
     }
     
     
-    public String createPatient() {ty
+    public String createPatient() {
         int created = facade.createPatient(patient);
         RequestContext context = RequestContext.getCurrentInstance();
         if (created == 1) {
