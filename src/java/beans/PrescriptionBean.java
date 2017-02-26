f41969bce46ebcc9cd2b18d1903e22d4c5e9b5ca
Oliivier <olivier.chitthavong@gmail.com>
2017-02-27 00:18:54 +0100

    Add the prescription for a patient.

---
+++ src/java/beans/PrescriptionBean.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import entities.PrescriptionEntity;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.context.RequestContext;
+import sessions.PrescriptionEntityFacade;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.el.ELContext;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import sessions.HasprescriptionEntityFacade;
+/**
+ *
+ * @author Olivier
+ */
+@Named(value = "prescriptionBean")
+@SessionScoped
+@ManagedBean
+public class PrescriptionBean implements Serializable {
+
+    @EJB
+    PrescriptionEntityFacade facadePrescription;
+    
+    @EJB
+    HasprescriptionEntityFacade facadeHasPrescription;
+    
+    private PrescriptionEntity prescription;    
+    
+    private String mailuser;    
+    private String socialSecurityId;
+    
+    @ManagedProperty(value="#{userBean}")
+    private UserBean user;
+    
+    @ManagedProperty(value="#{patientBean}")
+    private PatientBean patient;
+    
+    
+    public String getMailuser() {
+        return mailuser;
+    }
+
+    public String getSocialSecurityId() {
+        return socialSecurityId;
+    }
+   
+    public void setMailuser(String mailuser) {
+        this.mailuser = mailuser;
+    }
+
+    public void setSocialSecurityId(String socialSecurityId) {
+        this.socialSecurityId = socialSecurityId;
+    }
+    /**
+     * Creates a new instance of PrescriptionBean
+     */
+    public PrescriptionBean() {
+        this.prescription = new PrescriptionEntity();
+    }
+    
+    public PrescriptionEntity getPrescription(){
+        return prescription;
+    }
+    
+    public void setPrescription(PrescriptionEntity prescription){
+        this.prescription = prescription;
+    }
+    
+    public void setDate(){      
+        prescription.setDatePrescription(new java.util.Date());
+    }
+      
+    public String createPrescription() {
+        setDate();
+        prescription.setIdPrescription(ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE));
+        boolean created = facadePrescription.createPrescription(prescription);
+        facadeHasPrescription.createHasPrescription(socialSecurityId,prescription.getIdPrescription(), mailuser);
+        RequestContext context = RequestContext.getCurrentInstance();     
+        context.execute("swal('Félicitations','prescription créé','success')");
+        this.prescription = new PrescriptionEntity();
+        return "resultPatient";
+        
+    }
+}
