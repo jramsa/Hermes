28927d472bb88acc7b75c7de3c38a3ac2e6716e7
jramsa <jerome.ramsamy@hotmail.fr>
2017-03-02 01:06:53 +0100

    fix : add user to service

--- src/java/beans/HubBean.java
+++ src/java/beans/HubBean.java
@@ -35,18 +35,48 @@ public class HubBean implements Serializable {
     
     private String nameHosp;
     private String nameHub;
+    private String nameServ;
+    private String nameUser;
     
     public String addHubToHospital(){
         int created = hubFacade.addHubToHospital(nameHosp,nameHub);
         RequestContext context = RequestContext.getCurrentInstance();
         if (created != 0) {
-            context.execute("swal('Félicitations','Service ajouté','success')");
+            context.execute("swal('Félicitations','Pôle ajouté','success')");
             setNameHosp("");
             setNameHub("");
             return "addhub";
         } else {
-            context.execute("swal('Oups...','Service déjà ajouté','error')");
+            context.execute("swal('Oups...','Pôle déjà ajouté','error')");
             return "addhub";
+        }   
+    }
+    
+    public String addServToHub(){
+        int created = hubFacade.addServToHub(nameHosp,nameHub,nameServ);
+        RequestContext context = RequestContext.getCurrentInstance();
+        if (created != 0) {
+            context.execute("swal('Félicitations','Service ajouté','success')");
+            setNameHosp("");
+            setNameHub("");
+            return "addserv";
+        } else {
+            context.execute("swal('Oups...','Service déjà ajouté','error')");
+            return "addserv";
+        }   
+    }
+    
+    public String addUserToServ(){
+        int created = hubFacade.addUserToServ(nameServ,nameUser);
+        RequestContext context = RequestContext.getCurrentInstance();
+        if (created != 0) {
+            context.execute("swal('Félicitations','Utilisateur ajouté','success')");
+            setNameHosp("");
+            setNameHub("");
+            return "changeuserserv";
+        } else {
+            context.execute("swal('Oups...','Utilisateur déjà ajouté','error')");
+            return "changeuserserv";
         }   
     }
     
@@ -60,6 +90,24 @@ public class HubBean implements Serializable {
     
     public void onHubinHasHubChange() {
             setListHubinHasHub(hubFacade.aGetListHubinHasHub(getNameHosp()));
+    }
+    
+    private List<Object[]> listServ = new ArrayList<>();
+    
+    public void onServinHasServChange() {
+            listServ = hubFacade.aGetListServ();
+    }
+    
+    private List<Object[]> listServHasServ = new ArrayList<>();
+    
+    public void onListServHasServChange() {
+            listServHasServ = hubFacade.onListServHasServChange(nameHub);
+    }
+    
+    private List<Object[]> listUser = new ArrayList<>();
+    
+    public void onListUserChange() {
+            listUser = hubFacade.onListUserChange();
     }
     
     public String removeHubToHospital(){
@@ -154,6 +202,76 @@ public class HubBean implements Serializable {
      */
     public void setListHubinHasHub(List<Object[]> listHubinHasHub) {
         this.listHubinHasHub = listHubinHasHub;
+    }
+
+    /**
+     * @return the nameServ
+     */
+    public String getNameServ() {
+        return nameServ;
+    }
+
+    /**
+     * @param nameServ the nameServ to set
+     */
+    public void setNameServ(String nameServ) {
+        this.nameServ = nameServ;
+    }
+
+    /**
+     * @return the listServ
+     */
+    public List<Object[]> getListServ() {
+        return listServ;
+    }
+
+    /**
+     * @param listServ the listServ to set
+     */
+    public void setListServ(List<Object[]> listServ) {
+        this.listServ = listServ;
+    }
+
+    /**
+     * @return the nameUser
+     */
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    /**
+     * @param nameUser the nameUser to set
+     */
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    /**
+     * @return the listServHasServ
+     */
+    public List<Object[]> getListServHasServ() {
+        return listServHasServ;
+    }
+
+    /**
+     * @param listServHasServ the listServHasServ to set
+     */
+    public void setListServHasServ(List<Object[]> listServHasServ) {
+        this.listServHasServ = listServHasServ;
+    }
+
+    /**
+     * @return the listUser
+     */
+    public List<Object[]> getListUser() {
+        return listUser;
+    }
+
+    /**
+     * @param listUser the listUser to set
+     */
+    public void setListUser(List<Object[]> listUser) {
+        this.listUser = listUser;
     }
     
 }
--- src/java/sessions/HubEntityFacade.java
+++ src/java/sessions/HubEntityFacade.java
@@ -46,6 +46,16 @@ public class HubEntityFacade extends AbstractFacade<HubEntity> {
         Query query = em.createNativeQuery("INSERT INTO hasHub (hospitalName, idHub) VALUES ('"+ nameHosp +"', (SELECT idHub FROM Hub WHERE hubName='"+ nameHub + "'))");
         return query.executeUpdate();    
     }
+      
+      public int addServToHub(String nameHosp,String nameHub, String nameServ){
+        Query query = em.createNativeQuery("INSERT INTO hasService(idHub,idService) VALUES ((SELECT h.idHub FROM Hub h, Hospital hosp, hasHub WHERE h.hubName ='"+nameHub+ "' AND h.idHub=hasHub.idHub AND hasHub.hospitalName=hosp.hospitalName and hosp.hospitalName= '"+nameHosp+ "' ), (SELECT idService FROM Service s WHERE s.serviceName='"+ nameServ+ "'))");
+        return query.executeUpdate();    
+    }
+      
+      public int addUserToServ(String nameServ, String nameUser){
+        Query query = em.createNativeQuery("INSERT INTO WorkIn(mailUser,idService) VALUES ('"+nameUser+ "', (SELECT idService FROM Service s WHERE s.serviceName='"+ nameServ+ "'))");
+        return query.executeUpdate();    
+    }
     
       public List<Object[]> aGetListHospinHasHub(){
         Query query = em.createNativeQuery("SELECT DISTINCT hospitalName FROM hasHub");
@@ -56,9 +66,25 @@ public class HubEntityFacade extends AbstractFacade<HubEntity> {
         Query query = em.createNativeQuery("SELECT hubName FROM Hub h, hasHub has WHERE h.idHub=has.idHub AND has.hospitalName = '"+ nameHosp +"'");
         return query.getResultList();    
     }
+      
+      public List<Object[]> onListServHasServChange(String nameHub){
+        Query query = em.createNativeQuery("SELECT serviceName FROM Service s, hasService has, Hub h WHERE s.idService=has.idService AND has.idHub=h.idHub AND hubName = '"+ nameHub +"'");
+        return query.getResultList();    
+    }
+      
       public int removeHubToHospital(String nameHosp,String nameHub){
         Query query = em.createNativeQuery("DELETE FROM hasHub WHERE hospitalName='"+ nameHosp +"' AND idHub=(SELECT idHub from Hub WHERE Hub.hubName='"+ nameHub+ "')");
         return query.executeUpdate();    
+    }
+      
+      public List<Object[]> aGetListServ(){
+        Query query = em.createNativeQuery("SELECT serviceName FROM Service");
+        return query.getResultList();    
+    }
+      
+      public List<Object[]> onListUserChange(){
+        Query query = em.createNativeQuery("SELECT mailUser FROM User");
+        return query.getResultList();    
     }
       
 }
